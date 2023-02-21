9d97e24ee6f0041ba02a74e64096ce6a87be567e
Budzserator <borokbora@gmail.com>
2023-02-21 16:33:08 +0100

    Currently working on Header... PictureUpload ~ Done, CVUpload ~ Done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import CTA from './CTA'
+import ME from '../../assets/Barni03.jpg'
 
 function Header() {
   return (
@@ -7,6 +9,11 @@ function Header() {
         <h5> Hello I'm </h5>
         <h1> Barnabás Bor </h1>
         <h5 className='text-light'> Fullstack Developer </h5>
+        <CTA/>
+        <div className='me'>
+          <img src={ME} alt=''/> 
+        </div>
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
     </header>
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://www.linkedin.com/in/barnab%C3%A1s-bor-357184244/" target={'_blank'}></a>
+        <a href="https://github.com/Budzserator" target={'_blank'}></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
