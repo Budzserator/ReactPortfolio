f654e8b7fd9d34b567c1f9b010dcfbcdf9eb254b
Budzserator <borokbora@gmail.com>
2023-03-01 14:40:12 +0100

    Modification ~ Remove services from the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav"; 
 import About from "./components/about/About"
 import Experience from './components/experience/Experience';
-import Services from './components/services/Services'
 import Contact from './components/contact/Contact';
 
 const App = () => {
@@ -13,7 +12,6 @@ const App = () => {
       <Nav/>
       <About/>
       <Experience/>
-      <Services/>
       <Contact/>
     </>
   )
