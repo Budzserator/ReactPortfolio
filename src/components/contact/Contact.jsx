50e874edd6090b88edcf54865a2254ba1b19bfbb
Budzserator <borokbora@gmail.com>
2023-03-01 16:42:15 +0100

    Modify my project ~Remove unnecessary parts and complete the Contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
 import "./contact.css" 
+import {AiOutlineMail} from 'react-icons/ai'
+import {RiMessengerLine} from 'react-icons/ri' 
+import {AiOutlineWhatsApp} from 'react-icons/ai'
 
 function Contact() {
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <AiOutlineMail/>
+            <h4>Email</h4>
+            <h5>borokbora@gmail.com</h5>
+            <a href='mailto:borokbora@gmail.com' target='_blank'> Send an email</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine/>
+            <h4>Messenger</h4>
+            <h5>Barnabás Bor</h5>
+            <a href='https://m.me/barnabas.bor' target='_blank'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <AiOutlineWhatsApp/>
+            <h4>WhatsApp</h4>
+            <h5>+36709426248</h5>
+            <a href='https://api.whatsapp.com/send?phone=+36709426248' target='_blank'>Send a message</a>
+          </article>
+        </div>
+        <form action=''>
+          <input type='text' name='name' placeholder='Your Full Name' required/>
+          <input type='email' name='email' placeholder='Your Email' required/>
+          <textarea name='message' rows='7' placeholder='Your Message' required/>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>  
+      </div>
+    </section>
   )
 }
 
