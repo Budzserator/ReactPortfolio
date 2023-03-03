1d1e5510411bb0c8d10d7f80f945c6c6e4a44160
Budzserator <borokbora@gmail.com>
2023-03-03 13:46:10 +0100

    Footer component ~ Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "./components/nav/Nav";
 import About from "./components/about/About"
 import Experience from './components/experience/Experience';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <About/>
       <Experience/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./footer.css";
 
-function Footer() {
+const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <footer>
+      <a href="#home" className="footer__logo">
+        Barni's Portfolio Page 
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
