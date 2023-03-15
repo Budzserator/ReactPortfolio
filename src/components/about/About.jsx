a8da8a41eb28f0d9c535c9f5777e6b0a1ed4f9cf
Budzserator <borokbora@gmail.com>
2023-03-15 17:32:21 +0100

    Reformat code

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,49 +1,50 @@
 import React from 'react'
-import './about.css' 
-import ME from '../../assets/Barni02V.jpg' 
+import './about.css'
+import ME from '../../assets/Barni02V.jpg'
 import {AiOutlineExperiment} from 'react-icons/ai'
 import {BsFillPersonCheckFill} from 'react-icons/bs'
 import {AiOutlineProject} from 'react-icons/ai'
 
 function About() {
-  return (
-    <section id='about'>
-      <h5> Get to Know </h5>
-      <h2> About Me </h2>
+    return (
+        <section id='about'>
+            <h5> Get to Know </h5>
+            <h2> About Me </h2>
 
-    <div className='container about__container'>
-      <div className='about__me'>
-        <div className='about__me-image'>
-          <img src={ME} alt="About Image"/>
-        </div>
-      </div>
-      <div className='about__content'>
-        <div className='about__cards'>
-          <article className='about__card'>
-            <AiOutlineExperiment className='about__icon'/>
-            <h5>Programming Journey</h5>
-            <small> 1 year as a student of CodeCool </small>
-          </article>
+            <div className='container about__container'>
+                <div className='about__me'>
+                    <div className='about__me-image'>
+                        <img src={ME} alt="About Image"/>
+                    </div>
+                </div>
+                <div className='about__content'>
+                    <div className='about__cards'>
+                        <article className='about__card'>
+                            <AiOutlineExperiment className='about__icon'/>
+                            <h5>Programming Journey</h5>
+                            <small> 1 year as a student of CodeCool </small>
+                        </article>
 
-          <article className='about__card'>
-            <BsFillPersonCheckFill className='about__icon'/>
-            <h5>Further studies</h5>
-            <small> Currently studying to get a BsC in Computer Science. (Online)</small>
-          </article>
+                        <article className='about__card'>
+                            <BsFillPersonCheckFill className='about__icon'/>
+                            <h5>Further studies</h5>
+                            <small> Currently studying to get a BsC in Computer Science. (Online)</small>
+                        </article>
 
-          <article className='about__card'>
-            <AiOutlineProject className='about__icon'/>
-            <h5>Projects</h5>
-            <small> 50 + projects on the way becoming a junior...</small>
-          </article>  
-        </div>
-        <p> ~ Why hire me? ~ <br/>Because I am a hardworking reliable person who always give the best and also wants to improve.</p>
+                        <article className='about__card'>
+                            <AiOutlineProject className='about__icon'/>
+                            <h5>Projects</h5>
+                            <small> 50 + projects on the way becoming a junior...</small>
+                        </article>
+                    </div>
+                    <p> ~ Why hire me? ~ <br/>Because I am a hardworking reliable person who always give the best and
+                        also wants to improve.</p>
 
-        <a href='#contact' className='btn btn-primary'>Let's talk</a>
-      </div>
-    </div>
-    </section>
-  )
+                    <a href='#contact' className='btn btn-primary'>Let's talk</a>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default About
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,52 +1,52 @@
 import React from 'react'
-import "./contact.css" 
+import "./contact.css"
 import {AiOutlineMail} from 'react-icons/ai'
 import {RiMessengerLine} from 'react-icons/ri'
-import { useRef } from 'react' 
-import emailjs from 'emailjs-com' 
+import {useRef} from 'react'
+import emailjs from 'emailjs-com'
 
 function Contact() {
-  const form = useRef();
+    const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-    emailjs.sendForm('service_l7qe3dk', 'template_dqibvhc', form.current, '7gRKxTiXO1EvyVv1W')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      })
-      e.target.reset();
-  };
-  return (
-    <section id="contact">
-      <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
-      <div className='container contact__container'>
-        <div className='contact__options'>
-          <article className='contact__option'>
-            <AiOutlineMail/>
-            <h4>Email</h4>
-            <h5>borokbora@gmail.com</h5>
-            <a href='mailto:borokbora@gmail.com' target='_blank'> Send an email</a>
-          </article>
-          <article className='contact__option'>
-            <RiMessengerLine/>
-            <h4>Messenger</h4>
-            <h5>Barnabás Bor</h5>
-            <a href='https://m.me/barnabas.bor' target='_blank'>Send a message</a>
-          </article>
-        </div>
-        <form ref={form} onSubmit={sendEmail}>
-          <input type='text' name='name' placeholder='Your Full Name' required/>
-          <input type='email' name='email' placeholder='Your Email' required/>
-          <textarea name='message' rows='7' placeholder='Your Message' required/>
-          <button type='submit' className='btn btn-primary'>Send Message</button>
-        </form>  
-      </div>
-    </section>
-  )
+        emailjs.sendForm('service_l7qe3dk', 'template_dqibvhc', form.current, '7gRKxTiXO1EvyVv1W')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+        e.target.reset();
+    };
+    return (
+        <section id="contact">
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className='container contact__container'>
+                <div className='contact__options'>
+                    <article className='contact__option'>
+                        <AiOutlineMail/>
+                        <h4>Email</h4>
+                        <h5>borokbora@gmail.com</h5>
+                        <a href='mailto:borokbora@gmail.com' target='_blank'> Send an email</a>
+                    </article>
+                    <article className='contact__option'>
+                        <RiMessengerLine/>
+                        <h4>Messenger</h4>
+                        <h5>Barnabás Bor</h5>
+                        <a href='https://m.me/barnabas.bor' target='_blank'>Send a message</a>
+                    </article>
+                </div>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type='text' name='name' placeholder='Your Full Name' required/>
+                    <input type='email' name='email' placeholder='Your Email' required/>
+                    <textarea name='message' rows='7' placeholder='Your Message' required/>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                </form>
+            </div>
+        </section>
+    )
 }
 
 export default Contact
